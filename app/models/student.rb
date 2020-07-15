48ed4ac4068efc8222df3647f6bcfec8b34394f4
Osong Agberndifor <agberndifor7@gmail.com>
2020-07-15 10:37:18 +0100

    Add students model to app

---
+++ app/models/student.rb
@@ -0,0 +1,8 @@
+class Student < ApplicationRecord
+  before_save { self.email = email.downcase}
+  validates :name, presence: true, length: {minimum:5, maximum:35}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: 105},
+             uniqueness: { case_sensitive: false },
+             format: {with: VALID_EMAIL_REGEX}
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_15_080838) do
+ActiveRecord::Schema.define(version: 2020_07_15_090628) do
 
   create_table "courses", force: :cascade do |t|
     t.string "course_code"
     t.string "name"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
